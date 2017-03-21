95d7f86ab1cabb48c8c5bb55d4117d6998af7bb6
LuisPalominoTrevilla <luispalominot3@gmail.com>
2017-03-20 23:46:12 -0600

    Finished basic structure

--- CalculoISR/src/Ingresos.java
+++ CalculoISR/src/Ingresos.java
@@ -1,8 +1,13 @@
 public class Ingresos {
-	private Empleado empleado;
+	//private Empleado empleado;
 	private double sueldoMensual;
 	private double aguinaldo;
+	private double aguinaldoExcento;
+	private double aguinaldoGravado;
 	private double primaVacacional;
+	private double primaVacacionalExenta;
+	private double primaVacacionalGravada;
+	
 	
 }
 
