5b6ecad1ee87100428825a1961f11130f1bc783d
LuisPalominoTrevilla <luispalominot3@gmail.com>
2017-03-23 11:07:03 -0600

    .

--- CalculoISR/src/Pruebas.java
+++ CalculoISR/src/Pruebas.java
@@ -2,12 +2,19 @@
 public class Pruebas {
 
 	public static void main(String[] args) {
-		Empresa e = new Empresa(3);
-		System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
-		System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
-		System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
-		System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
+		// Empresa e = new Empresa(3);
+		// System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
+		// System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
+		// System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
+		// System.out.println(e.addEmpleado("luis", "dsfd", new Deduccion(), new Ingresos()));
 
+		Ingresos test = new Ingresos(30000,30000,1600);
+		System.out.println(test.getSueldoAnual());
+		System.out.println(test.getAguinaldoGravado());
+		System.out.println(test.getAguinaldoExcento());
+		System.out.println(test.getPrimaVacacionalGravada());
+		System.out.println(test.getPrimaVacacionalExcenta());
+		System.out.println(test.getTotalIngresos());
+		System.out.println(test.getIngresosGravados());
 	}
-
 }
